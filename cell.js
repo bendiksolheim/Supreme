7cd28d79da0f1719a963e50619f67823e400a1b1
Bendik Solheim <bendik.solheim@bekk.no>
2014-04-19 21:59:48 +0200

    You now have to double click instead of single click to edit. Enter now blurs element instead of creating newline

--- cell.js
+++ cell.js
@@ -1,6 +1,6 @@
 (function(window) {
-	function Cell(data) {
-		this.data = data;
+	function Cell(value) {
+		this.value = value;
 		this.element = this.__createElement();
 	}
 
@@ -8,28 +8,33 @@
 		var td = document.createElement('td');
 		td.className = 'editable';
 		td.tabIndex = '-1';
-		td.addEventListener('click', this, false);
+		td.addEventListener('dblclick', this, false);
 		td.addEventListener('focusout', this, false);
 		td.addEventListener('change', this, false);
+		td.addEventListener('keypress', this, false);
 		return td;
 	};
 
 	Cell.prototype.handleEvent = function(event) {
 		switch (event.type) {
-			case 'click':
-				this.click();
+			case 'dblclick':
+				this.edit();
 				break;
 			case 'focusout':
 				this.unfocus();
+				break;
 			case 'change':
 				this.change(this.element.innerHTML);
 				break;
+			case 'keypress':
+				this.keypressed(event);
+				break;
 		}
-		console.log(event);
 	};
 
-	Cell.prototype.click = function() {
+	Cell.prototype.edit = function(event) {
 		this.element.contentEditable = true;
+		this.element.focus();
 	};
 
 	Cell.prototype.unfocus = function() {
@@ -37,8 +42,24 @@
 	};
 
 	Cell.prototype.change = function(value) {
-		this.data = value;
+		this.value = value;
 		this.element.innerHTML = value;
+	};
+
+	Cell.prototype.activate = function() {
+		var event = new MouseEvent('dblclick', {
+			'view': window,
+			'bubbles': true,
+			'cancelable': true
+		});
+		this.element.dispatchEvent(event);
+	};
+
+	Cell.prototype.keypressed = function(event) {
+		var keyCode = event.keyCode;
+		if (keyCode == 13) {
+			this.element.blur();
+		}
 	};
 
 	window.Cell = Cell;
